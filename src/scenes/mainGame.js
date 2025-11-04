8e52218fffe60357329a1bd30560c71a9fcf3ca0
Jasper <jlmkowski@icloud.com>
2025-11-03 19:23:20 -0500

    Removed the ability to cast while already casted and set maximum cast length to 500

--- src/scenes/mainGame.js
+++ src/scenes/mainGame.js
@@ -96,40 +96,44 @@ export default class MainGame extends Phaser.Scene{
 
         // cast(length)
         C4C.Interpreter.define('cast', (length) => {
-            if (length === undefined) {
-                length = 100;
+            if(this.moveFreely){
+                if (length === undefined) {
+                    length = 100;
+                } else if (length > 500){
+                    length = 500;
+                }
+                // Create hook
+                if (this.rightFacing){
+                    this.hook = this.physics.add.sprite(this.player.x + 90, this.player.y - 60, 'hook').setDisplaySize(30,30);
+                } else {
+                    this.hook = this.physics.add.sprite(this.player.x - 90, this.player.y - 60, 'hook').setDisplaySize(30,30);
+                }
+                // Stop/freeze player movements
+                    this.moveFreely = false;
+
+                // Move hook down according to length arg
+                    this.tweens.add({
+                        targets: this.hook, // The sprite you want to move
+                        y: length + 100,           // The target Y coordinate
+                        duration: 1000,       // Duration of the tween in milliseconds
+                        ease: 'Power2',       // Easing function for smoother animation (optional)
+                        onComplete: () => {
+                            // Code to execute when the sprite reaches the target position
+                            //Detect Collisions:
+                                //ADD CODE
+                            // Waiting Period, then resume game
+                                this.timedEvent = this.time.delayedCall(3000, resumeGame, null, this);
+                        }
+                    });
+
+                // Stop after 1 game loop
+                setTimeout(() => {
+                    try {
+                    // ?
+
+                    } catch (e){};
+                }, gameLoopSpeed);
             }
-            // Create hook
-            if (this.rightFacing){
-                this.hook = this.physics.add.sprite(this.player.x + 90, this.player.y - 60, 'hook').setDisplaySize(30,30);
-            } else {
-                this.hook = this.physics.add.sprite(this.player.x - 90, this.player.y - 60, 'hook').setDisplaySize(30,30);
-            }
-            // Stop/freeze player movements
-                this.moveFreely = false;
-
-            // Move hook down according to length arg
-                this.tweens.add({
-                    targets: this.hook, // The sprite you want to move
-                    y: length + 100,           // The target Y coordinate
-                    duration: 1000,       // Duration of the tween in milliseconds
-                    ease: 'Power2',       // Easing function for smoother animation (optional)
-                    onComplete: () => {
-                        // Code to execute when the sprite reaches the target position
-                        //Detect Collisions:
-                            //ADD CODE
-                        // Waiting Period, then resume game
-                            this.timedEvent = this.time.delayedCall(3000, resumeGame, null, this);
-                    }
-                });
-
-            // Stop after 1 game loop
-            setTimeout(() => {
-                try {
-                  // ?
-
-                } catch (e){};
-            }, gameLoopSpeed);
         })
  
     // ----------------------------------------------------------------
